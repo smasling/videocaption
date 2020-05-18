a5e61a24cdce2e2c6c8e736be39782ada500dea2
Sam Masling <sam.masling@gmail.com>
2020-05-17 21:22:19 -0400

    downsample by 10

--- utils.py
+++ utils.py
@@ -10,16 +10,16 @@ def conv_video_to_frames(s, train, count):
     frameWidth = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     frameHeight = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     print(frameCount, frameHeight, frameWidth )
-    buf = np.empty((int(frameCount/8), frameHeight, frameWidth, 3), np.dtype('uint8'))
+    buf = np.empty((int(frameCount/10), frameHeight, frameWidth, 3), np.dtype('uint8'))
     fff = 0
     ret = True
     while (fff < frameCount and ret):
         ret, temp = cap.read()
-        if fff % 8 == 0:
+        if fff % 10 == 0:
             buf[fff] = temp
         fff += 1
     cap.release()
-    curr = np.pad(buf, (0, (113 - buf.shape[0]), (0,0), (0,0), (0,0)))
+    curr = np.pad(buf, (0, (90 - buf.shape[0]), (0,0), (0,0), (0,0)))
     del buf
     del curr
     print(count)
@@ -29,7 +29,7 @@ def conv_video_to_frames(s, train, count):
 
 def create_train_videos():
     vids = [f[:-4] for f in listdir('testFolder')]
-    train = np.zeros((len(vids), 113, 240, 320, 3), dtype=np.uint8)
+    train = np.zeros((len(vids), 90, 240, 320, 3), dtype=np.uint8)
     count = 0
     for v in vids:
         file = "testFolder/" + v + ".mp4"
